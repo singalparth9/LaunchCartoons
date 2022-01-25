1c267f502e0cebb24ac42f24958924ca2fd559f4
singalparth9 <94890275+singalparth9@users.noreply.github.com>
2022-01-25 18:00:07 +0530

    Add files via upload

---
+++ hard.js
@@ -0,0 +1,141 @@
+function hideimage() {
+    document.getElementById("mp").style.display="none";
+    document.getElementById("cb").style.display="none";
+    document.getElementById("tom").style.display="none";
+    document.getElementById("chbh").style.display="none";
+    document.getElementById("roll").style.display="none";
+    document.getElementById("little").style.display="none";
+    document.getElementById("shinchan").style.display="none";
+    document.getElementById("oggy").style.display="none";
+    document.getElementById("honey").style.display="none";
+    document.getElementById("pakdam").style.display="none";
+}
+
+function showimage(id) {
+    if(id=='btnMotu'){
+    document.getElementById("mp").style.display="block";
+    document.getElementById("cb").style.display="none";
+    document.getElementById("tom").style.display="none";
+    document.getElementById("chbh").style.display="none";
+    document.getElementById("roll").style.display="none";
+    document.getElementById("little").style.display="none";
+    document.getElementById("shinchan").style.display="none";
+    document.getElementById("oggy").style.display="none";
+    document.getElementById("honey").style.display="none";
+    document.getElementById("pakdam").style.display="none";
+    }
+    if(id=='btnChota'){
+    document.getElementById("cb").style.display="block";
+    document.getElementById("mp").style.display="none"; 
+    document.getElementById("tom").style.display="none";
+    document.getElementById("chbh").style.display="none";
+    document.getElementById("roll").style.display="none";
+    document.getElementById("little").style.display="none";
+    document.getElementById("shinchan").style.display="none";
+    document.getElementById("oggy").style.display="none";
+    document.getElementById("honey").style.display="none";
+    document.getElementById("pakdam").style.display="none";
+    }
+if(id=='btnTom'){
+    document.getElementById("cb").style.display="none";
+    document.getElementById("mp").style.display="none"; 
+    document.getElementById("tom").style.display="block";
+    document.getElementById("chbh").style.display="none";
+    document.getElementById("roll").style.display="none";
+    document.getElementById("little").style.display="none";
+    document.getElementById("shinchan").style.display="none";
+    document.getElementById("oggy").style.display="none";
+    document.getElementById("honey").style.display="none";
+    document.getElementById("pakdam").style.display="none";
+}
+
+if (id=="btnChacha") {
+    document.getElementById("cb").style.display="none";
+    document.getElementById("mp").style.display="none"; 
+    document.getElementById("tom").style.display="none";
+    document.getElementById("chbh").style.display="block";
+    document.getElementById("roll").style.display="none";
+    document.getElementById("little").style.display="none";
+    document.getElementById("shinchan").style.display="none";
+    document.getElementById("oggy").style.display="none";
+    document.getElementById("honey").style.display="none";
+    document.getElementById("pakdam").style.display="none";
+}   
+if (id=="btnRoll") {
+    document.getElementById("cb").style.display="none";
+    document.getElementById("mp").style.display="none"; 
+    document.getElementById("tom").style.display="none";
+    document.getElementById("chbh").style.display="none";
+    document.getElementById("roll").style.display="block";
+    document.getElementById("little").style.display="none";
+    document.getElementById("shinchan").style.display="none";
+    document.getElementById("oggy").style.display="none";
+    document.getElementById("honey").style.display="none";
+    document.getElementById("pakdam").style.display="none";
+}
+if (id=="btnKri") {
+    document.getElementById("cb").style.display="none";
+    document.getElementById("mp").style.display="none"; 
+    document.getElementById("tom").style.display="none";
+    document.getElementById("chbh").style.display="none";
+    document.getElementById("roll").style.display="none";
+    document.getElementById("little").style.display="block";
+    document.getElementById("shinchan").style.display="none";
+    document.getElementById("oggy").style.display="none";
+    document.getElementById("honey").style.display="none";
+    document.getElementById("pakdam").style.display="none";
+}
+if (id=="btnShi") { 
+    document.getElementById("cb").style.display="none";
+    document.getElementById("mp").style.display="none"; 
+    document.getElementById("tom").style.display="none";
+    document.getElementById("chbh").style.display="none";
+    document.getElementById("roll").style.display="none";
+    document.getElementById("little").style.display="none";
+    document.getElementById("shinchan").style.display="block";
+    document.getElementById("oggy").style.display="none";
+    document.getElementById("honey").style.display="none";
+    document.getElementById("pakdam").style.display="none";
+}
+if (id=="btnOggy") {
+    document.getElementById("cb").style.display="none";
+    document.getElementById("mp").style.display="none"; 
+    document.getElementById("tom").style.display="none";
+    document.getElementById("chbh").style.display="none";
+    document.getElementById("roll").style.display="none";
+    document.getElementById("little").style.display="none";
+    document.getElementById("shinchan").style.display="none";
+    document.getElementById("oggy").style.display="block";
+    document.getElementById("honey").style.display="none";
+    document.getElementById("pakdam").style.display="none";
+}
+if (id=="btnBunny") {
+    document.getElementById("cb").style.display="none";
+    document.getElementById("mp").style.display="none"; 
+    document.getElementById("tom").style.display="none";
+    document.getElementById("chbh").style.display="none";
+    document.getElementById("roll").style.display="none";
+    document.getElementById("little").style.display="none";
+    document.getElementById("shinchan").style.display="none";
+    document.getElementById("oggy").style.display="none";
+    document.getElementById("honey").style.display="block";
+    document.getElementById("pakdam").style.display="none";
+}
+if (id=="btnPak") {
+    document.getElementById("cb").style.display="none";
+    document.getElementById("mp").style.display="none"; 
+    document.getElementById("tom").style.display="none";
+    document.getElementById("chbh").style.display="none";
+    document.getElementById("roll").style.display="none";
+    document.getElementById("little").style.display="none";
+    document.getElementById("shinchan").style.display="none";
+    document.getElementById("oggy").style.display="none";
+    document.getElementById("honey").style.display="none";
+    document.getElementById("pakdam").style.display="block";
+}
+}
+
+
+
+
+
